35acf5a3c94667ad8e154d3022f6d6981727ed4d
Sushant <Sushant@DESKTOP-KKP5IKJ>
2023-11-25 06:01:10 +0530

    Registration Info

---
+++ wacartproject/src/main/java/com/test/Registration.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Registration {
+	
+	public static void main(String[] args) {
+	
+	System.out.println("personal Info : ABC ");
+	
+	System.out.println("Address Info : pune ");
+	
+	}
+}
